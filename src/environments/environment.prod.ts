8ef3889687072958b2daec17c2d891fe7360e8d6
unknown <wnethmi96@gmail.com>
2019-09-28 14:08:30 +0530

    Erised Works

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB1pPl7FHNJ-2n6ab6nEZqdLCct_AKkSw8',
+    authDomain: 'erised-works.firebaseapp.com',
+    databaseURL: 'https://erised-works.firebaseio.com',
+    projectId: 'erised-works',
+    storageBucket: '',
+    messagingSenderId: '95708226947',
+    appId: '1:95708226947:web:37563d2d17f6d789de42b6',
+    measurementId: 'G-3K0VHD9PQK'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB1pPl7FHNJ-2n6ab6nEZqdLCct_AKkSw8',
+    authDomain: 'erised-works.firebaseapp.com',
+    databaseURL: 'https://erised-works.firebaseio.com',
+    projectId: 'erised-works',
+    storageBucket: '',
+    messagingSenderId: '95708226947',
+    appId: '1:95708226947:web:37563d2d17f6d789de42b6',
+    measurementId: 'G-3K0VHD9PQK'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
